2d93fba43774a7898aa9770db543bb5243eedecc
Charli <charlisung@gmail.com>
2022-04-07 13:01:56 -0700

    change func names

---
+++ README.md
@@ -0,0 +1,18 @@
+# Calculator
+
+A calculator built with JavaScript. It has two screens: top for a result and bottom for showing the process.
+
+## 🛠 Skills
+
+HTML5, CSS, JavaScript
+
+## Lessons Learned
+
+- CSS grid
+- Switch statement
+- Type of e.target.value is string
+- isNaN function
+
+## Demo
+
+![calculator](calculator.gif)
--- app.js
+++ app.js
@@ -1,78 +1,88 @@
 const screen = document.querySelector(".screen");
 const trackScreen = document.querySelector(".history-screen");
 const btn = document.querySelector(".buttons");
+
 let total = 0;
 let screenText = "0";
-let history = [];
-let prevOperator;
+let historyArr = [];
+let prevOperator = null;
 
-// handling number input
+// handling number
 function handleNumber(num) {
   if (screenText === "0") {
-    screenText = num;
-    history.push(num);
+    if (num !== "0") {
+      screenText = num;
+      historyArr.push(num);
+    }
   } else {
     screenText += num;
-    history.push(num);
+    historyArr.push(num);
   }
 }
 
-// handling math symbols
+// handling math operators
 function handleSymbol(symbol) {
   if (screenText === "0") {
     return;
   }
+  // resetting variables values
   switch (symbol) {
     case "AC":
       total = 0;
       screenText = "0";
-      history = [];
+      historyArr = [];
       break;
     case "=":
       if (prevOperator === null) {
         return;
       }
-      getResult(parseInt(screenText));
+      handleMath(parseInt(screenText));
       // save new total value to screen text and resetting other values
       screenText = total;
       prevOperator = null;
       total = 0;
-      history = [];
-      history.push(screenText);
+      historyArr = [];
+      historyArr.push(screenText);
       break;
     case "+":
     case "-":
     case "*":
     case "/":
-      handleMath(symbol);
-      history.push(symbol);
+      handleBuffer(symbol);
+      historyArr.push(symbol);
       break;
   }
 }
 
-function handleMath(symbol) {
+function handleBuffer(symbol) {
   if (screenText === "0") {
     return;
   }
-  const intscreenText = parseInt(screenText);
+
+  const intScreenText = parseInt(screenText);
+
   if (total === 0) {
-    total = intscreenText;
+    total = intScreenText;
   } else {
-    getResult(intscreenText);
+    handleMath(intScreenText);
   }
+
+  // handleBuffer func is basically for assigning prevOperator and screenText
   prevOperator = symbol;
   screenText = "0";
 }
 
-function getResult(intscreenText) {
+// getting total result
+
+function handleMath(intScreenText) {
   if (prevOperator === "+") {
-    total += intscreenText;
+    total += intScreenText;
   } else if (prevOperator === "-") {
-    total -= intscreenText;
+    total -= intScreenText;
   } else if (prevOperator === "*") {
-    total *= intscreenText;
+    total *= intScreenText;
   } else {
-    total /= intscreenText;
+    total /= intScreenText;
   }
 }
 
@@ -83,5 +93,5 @@ btn.addEventListener("click", (e) => {
     handleNumber(e.target.value);
   }
   screen.innerText = screenText;
-  trackScreen.innerText = history.join(" ");
+  trackScreen.innerText = historyArr.join(" ");
 });
